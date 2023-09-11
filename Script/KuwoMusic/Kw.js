c104f478108ca1167097914375216a4cf2805fdc
General℡ <76241047+General74110@users.noreply.github.com>
2023-09-12 04:32:34 +0800

    Update Kw.js

--- Script/KuwoMusic/Kw.js
+++ Script/KuwoMusic/Kw.js
@@ -12,7 +12,7 @@
 当前版本:10.2.8
 [rewrite_local]
 # 完美解锁酷我音乐
-^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/General74110/QuantumultX/master/JS/Kuwo/Kw.js
+^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/KuwoMusic/Kw.js
 [mitm] 
 hostname = *.kuwo.cn, *.lrts.me
 ********************************

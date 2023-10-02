5e2e666a99ac92771a51f64565d491448af86672
General℡ <76241047+General74110@users.noreply.github.com>
2023-10-02 14:15:26 +0800

    更新 Cheji.js

--- Script/UnlockVip /Cheji.js
+++ Script/UnlockVip /Cheji.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 
-^https:\/\/tsp\.txzing\.com\/api(\/p/operation/common/allowFreeVip|\/p/operation/vipCoupon/popUpCouponInfo|\/p/operation/activity/situation|\/w/wx/module/findCar/findCarVipInfo/isVip|\/w/wx/module/trace/userVip) url script-response-body Cheji.js
+^https:\/\/tsp\.txzing\.com\/api(\/p/operation/common/allowFreeVip|\/p/operation/vipCoupon/popUpCouponInfo|\/p/operation/activity/situation|\/w/wx/module/findCar/findCarVipInfo/isVip|\/w/wx/module/trace/userVip|\/w/wx/module/traceMp/traceUserVipOpenId/vipInfo) url script-response-body Cheji.js
 
 
 MITM = tsp.txzing.com
@@ -16,6 +16,7 @@ const dish = '/vipCoupon/popUpCouponInfo';
 const sb = '/activity/situation';
 const bd = '/findCarVipInfo/isVip';
 const gj = '/trace/userVip';
+const gg = 'traceUserVipOpenId/vipInfo';
 
 if (url.indexOf(vip) != -1) {
    obj["data"] = "true";
@@ -43,4 +44,9 @@ if (url.indexOf(gj) != -1) {
 	body = JSON.stringify(obj);
     }
 
+if (url.indexOf(gg) != -1) {
+   obj["data"]["vip"] = "true";
+   obj["data"]["day"] = "738639";
+   body = JSON.stringify(obj);
+   }
 $done({body});

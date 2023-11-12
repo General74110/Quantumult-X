7acc00d1f46f4d4d69818dfd9c4ad801f28ca18f
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-12 19:11:01 +0800

    更新 lushui.js

--- Script/UnlockVip /lushui.js
+++ Script/UnlockVip /lushui.js
@@ -1,5 +1,7 @@
 /*
+General℡
 
+⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
 ^https:\/\/api\.wangmianhua\.cn\/api(\/user/info|\/vod/watch|\/vod/buy) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/lushui.js
 
@@ -8,40 +10,15 @@ hostname = api.wangmianhua.cn
 */
 
 var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+/*
+General℡
 
-const vod = '/api/vod/watch';
-const buy = '/api/vod/buy';
-const user = '/api/user/info';
+⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
-
-if (url.indexOf(buy) != -1) {
-   obj["result"] = "true";
-   obj["code"] = "200";
-   body = JSON.stringify(obj);
-   }
-   
-    
-if (url.indexOf(vod) != -1) {
-   obj["data"]["bought"] = "true"
-   obj["data"]["availableAmount"] = "9999"
-	body = JSON.stringify(obj);
-    }
+^https:\/\/api\.wangmianhua\.cn\/api(\/user/info|\/vod/watch|\/vod/buy) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/lushui.js
 
 
- if (url.indexOf(user) != -1) {
-	obj["data"]["vipType"] = "isvip"
-	obj["data"]["vipEndDate"] = "2099"
-	obj["data"]["vipEndYear"] = "2099"
-	obj["data"]["vipEndMonth"] = "9"
-	obj["data"]["vipEndDay"] = "9"
-	obj["data"]["vipEndHour"] = "9"
-	obj["data"]["avaliableAmount"] = "9999"
-   obj["data"]["recharged"] = "ture"
-   
-	
-	body = JSON.stringify(obj);
-    }
+hostname = api.wangmianhua.cn
+*/
 
-$done({body});
+dmFyIGJvZHkgPSAkcmVzcG9uc2UuYm9keTs=

d1d3c1db2e017146ddb8a13edb81effc4771b0a2
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-12 15:46:30 +0800

    更新 lushui.js

--- Script/UnlockVip /lushui.js
+++ Script/UnlockVip /lushui.js
@@ -5,7 +5,8 @@
 
 ^https:\/\/api\.wangmianhua\.cn\/api(\/user/info|\/vod/watch|\/vod/buy) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/lushui.js
 
-MITM = api.wangmianhua.cn
+MITM 
+host = api.wangmianhua.cn
 */
 
 var body = $response.body;

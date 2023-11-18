88805c483c3642016263ad602f46b7cb1857a717
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-19 00:53:57 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -1,7 +1,7 @@
 /*
 General℡
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
-^https:\/\/commontgw\.reader\.qq\.com\/v7_6_6\/ttsSpeakerList url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+^https:\/\/commontgw\.reader\.qq\.com(\/v7_6_6\/ttsSpeakerList|\/common\/monthpage) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
 
 
 
@@ -19,12 +19,21 @@ var obj = JSON.parse(body);
 
 
 const tt = 'v7_6_6\/ttsSpeakerList';
-
+const ts = 'common/monthpage';
 
 
 if (url.indexOf(tt) != -1) {
    obj["isVip"] = "1";
    
+   body = JSON.stringify(obj);
+   }
+   
+   if (url.indexOf(ts) != -1) {
+   obj["isVip"] = "1";
+   obj["endtimeYearVip"] = "2099";
+   obj["endtime"] = "4098703507";
+   obj["status"] = "1";
+   obj["balance"] = "16800";
    body = JSON.stringify(obj);
    }
    

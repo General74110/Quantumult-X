cd90d584f890d371b42af332e34380e1406967be
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-19 00:01:19 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -1,7 +1,7 @@
 /*
 General℡
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
-^https?:\/\/commontgw\.reader\.qq\.com(\/common\/adV3|\/v7_6_6\/nativepage\/getAcctInfo|\/v7_6_6\/nativepage\/personal|book\/queryBookInfo) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+^https:\/\/commontgw\.reader\.qq\.com\/book\/queryBookInfo\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
 
 
 
@@ -18,63 +18,30 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 
-const vipad = 'common/adV3';//vip去广告信息
-const vip = 'nativepage/getAcctInfo';
-const vip1 = 'v7_6_6/userinfo';
-const vip2 = 'v7_6_6/nativepage/personal';//vip用户信息
-const bookvip = 'book/queryBookInfo';
+const viprd = 'book/queryBookInfo';
 
 
-if (url.indexOf(vipad) != -1) {
-   obj["isVip"] = "true";
-   
-   body = JSON.stringify(obj);
-   }
-
-if (url.indexOf(vip) != -1) {
-   obj["norLevel"] = "10";
-   obj["isVip"] = "true";
-   obj["vipEndTime"] = "2050-09-09";
-   obj["vipType"] = "1";
-   obj["balance"] = "61800";
-   obj["vipStatus"] = "1";
-   obj["bookTicketEndtime"] = "2050-09-09";
-   obj["isMVip"] = "true";
-   obj["vipLevel"] = "10";
-   
-   
-   body = JSON.stringify(obj);
-   }
-
-if (url.indexOf(vip1) != -1) {
-   obj["isVip"] = "1";
-   obj["vipType"] = "1";
-   obj["endTime"] = "2546342064000";
-   obj["is_vip"] = "1";
-   obj["vipStatus"] = "1";
-   
-   
-   body = JSON.stringify(obj);
-   }
-   
-if (url.indexOf(vip2) != -1) {
-   obj["isVip"] = "true";
-   
-   body = JSON.stringify(obj);
-   }
-   
-if (url.indexOf(bookvip) != -1) {
+if (url.indexOf(viprd) != -1) {
    obj["isVip"] = "true";
    obj["gyBook"] = "1";
+   obj["is29903PirateBook"] = "true";
    obj["payinfo"] = "true";
-   obj["vipBookLeadType"] = 1;
+   obj["bookMonthlyEndTime"] = "2056-04-26 12:16:24";
+   obj["vipBookLeadType"] = "1";
+   obj["isAdBook"] = "true";
+   obj["is20002AdBook"] = "true";
    obj["chapterpayinfo"] = "true";
    obj["vipStatus"] = "1";
-   obj["islimitfreebook"] = "true";
+   obj["isPrecollection"] = "1";
+   obj["allowMonthlyPay"] = "1";
+   obj["limitfreechapterstime"] = "2014-02-12#2056-03-01";
+   obj["islimitfreebook"] ="true";
+   obj["needOpenVip"] = "false";
    
    
    body = JSON.stringify(obj);
    }
+
 
 
 $done({body}); 

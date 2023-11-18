470ce931fbe06c51601e1ed7e2267e4e1a92ac60
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-19 00:35:09 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -1,7 +1,7 @@
 /*
 General℡
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
-^https:\/\/commontgw\.reader\.qq\.com(\/book\/queryBookInfo|\/v7_6_6\/chapterOver) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+^https:\/\/commontgw\.reader\.qq\.com\/v7_6_6\/ttsSpeakerList url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
 
 
 
@@ -18,31 +18,11 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 
-const viprd = 'book/queryBookInfo';
-const vipp = 'v7_6_6/chapterOver';
+const tt = 'v7_6_6\/ttsSpeakerList';
 
-if (url.indexOf(viprd) != -1) {
-   obj["isVip"] = "true";
-   obj["gyBook"] = "1";
-   obj["is29903PirateBook"] = "true";
-   obj["payinfo"] = "true";
-   obj["bookMonthlyEndTime"] = "2056-04-26 12:16:24";
-   obj["vipBookLeadType"] = "1";
-   obj["isAdBook"] = "true";
-   obj["is20002AdBook"] = "true";
-   obj["chapterpayinfo"] = "true";
-   obj["vipStatus"] = "1";
-   obj["isPrecollection"] = "1";
-   obj["allowMonthlyPay"] = "1";
-   obj["limitfreechapterstime"] = "2014-02-12#2056-03-01";
-   obj["islimitfreebook"] ="true";
-   obj["needOpenVip"] = "false";
-   
-   
-   body = JSON.stringify(obj);
-   }
 
-if (url.indexOf(vipp) != -1) {
+
+if (url.indexOf(tt) != -1) {
    obj["isVip"] = "1";
    
    body = JSON.stringify(obj);

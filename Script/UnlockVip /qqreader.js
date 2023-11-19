ccf693fe467e191b431a4e94d1dc2da4c5ea1837
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-19 22:28:50 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -1,7 +1,7 @@
 /*
 General℡
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
-^https:\/\/commontgw\.reader\.qq\.com(\/v7_6_6\/ttsSpeakerList|\/common\/monthpage) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+^https?:\/\/commontgw\.reader\.qq\.com\/v7\_6\_6\/readonline\/new\/adInfo url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
 
 
 
@@ -18,23 +18,17 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 
-const tt = 'v7_6_6\/ttsSpeakerList';
-const ts = 'common/monthpage';
+const Info = 'new/adInfo';
 
 
 if (url.indexOf(tt) != -1) {
-   obj["isVip"] = "1";
+   obj["isVip"] = "true";
+   obj["buytype"] = "0";
+   
    
    body = JSON.stringify(obj);
    }
    
-   if (url.indexOf(ts) != -1) {
-   obj["isVip"] = "1";
-   obj["endtimeYearVip"] = "2099";
-   obj["endtime"] = "4098703507";
-   obj["status"] = "1";
-   obj["balance"] = "16800";
-   body = JSON.stringify(obj);
-   }
+   
    
 $done({body}); 

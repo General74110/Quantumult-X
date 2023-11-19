395fd159644671e36f93af20363e768b1345f5fb
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-19 22:51:15 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -1,7 +1,7 @@
 /*
 General℡
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
-^https?:\/\/commontgw\.reader\.qq\.com\/v7\_6\_6\/readonline\/new\/adInfo url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+^https?:\/\/commontgw\.reader\.qq\.com\/(v7\_6\_6\/readonline\/new\/adInfo|book\/queryBookInfo) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
 
 
 
@@ -19,12 +19,25 @@ var obj = JSON.parse(body);
 
 
 const Info = 'new/adInfo';
-
+const bookinfo = 'book/queryBookInfo';
 
 if (url.indexOf(Info) != -1) {
    obj["isVip"] = "true";
    obj["buytype"] = "0";
    obj["paid"] = "1";
+   
+if (url.indexOf(bookinfo) != -1) {
+   obj["isVip"] = "true";
+   obj["vipStatus"] = "1";
+   obj["gyBook"] = "1";   
+   obj["is29903PirateBook"] = "true";
+   obj["is20002AdBook"] = "true";
+   obj["isAdBook"] = "true";
+   obj["vipBookLeadType"] = "1";
+   obj["banTTS"] = "true";
+   obj["payinfo"] = "true";
+   obj["chapterpayinfo"] = "true";
+      
    
    body = JSON.stringify(obj);
    }

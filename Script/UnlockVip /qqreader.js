a984361664acfe52fe96170978a5e6383ee01a05
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-20 11:53:29 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -19,14 +19,18 @@ var obj = JSON.parse(body);
 
 
 const Info = 'new/adInfo';
-const yd = 'book/queryBookInfo';
+const BookInfo = 'book/queryBookInfo';
 
 if (url.indexOf(Info) != -1) {
    obj["isVip"] = "true";
    obj["buytype"] = "0";
    obj["paid"] = "1";
+  
    
-if (url.indexOf(yd) != -1) {
+    body = JSON.stringify(obj);
+   }
+   
+if (url.indexOf(BookInfo) != -1) {
    obj["isVip"] = "true";
    obj["vipStatus"] = "1";
    obj["gyBook"] = "1";   

cbbd0f75b400ce987c441f6e576e739b9b319c98
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-22 22:40:25 +0800

    更新 qqreaderAd.js

--- Script/UnlockVip /qqreaderAd.js
+++ Script/UnlockVip /qqreaderAd.js
@@ -24,30 +24,26 @@ let requestUrl = $request.url;
 // 判断是否为匹配项
 if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3/.test(requestUrl))//去除 我的-活动页面
 
+if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/firstOpenApp\/ad?/.test(requestUrl))
 
 {
 //判断是否存在数据
-if (obj.hasOwnProperty("data")) {
+if (obj.hasOwnProperty("data")) 
+if (obj.hasOwnProperty("sk"))
+{
 
 delete obj.data;//删除指定数据
 
 
 console.log("活动页面拦截成功！！！");//打印拦截结果
+
+delete obj.sk.pool;
+console.log("拦截成功！！！");
 }
 else{
     console.log("拦截失败！！！");
 }
-if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/firstOpenApp\/ad?/.test(requestUrl))//
-
-{
-    if (obj.hasOwnProperty("sk")) {
-         delete obj.sk.pool;
-        console.log("拦截成功!!!");
-        }
-        else{
-            console.log("拦截失败!!!");
-            }
-            }
+ 
 }
 //重写数据
 $done ({ body: JSON.stringify(obj) });

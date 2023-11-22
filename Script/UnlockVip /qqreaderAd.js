af4bbd683fd910687cb524bde35ba4ed84c413fc
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-22 21:41:57 +0800

    更新 qqreaderAd.js

--- Script/UnlockVip /qqreaderAd.js
+++ Script/UnlockVip /qqreaderAd.js
@@ -14,9 +14,9 @@ let obj = JSON.parse($response.body) ;
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/'^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3/.test(requestUrl)){
+if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3/.test(requestUrl)){
 //判断是否存在数据
-if (obj.hasOwnProperty("data"')) {
+if (obj.hasOwnProperty("data")) {
 
 delete obj.data;//删除指定数据
 

0fdce4e82208a2b773e57b2de859afb2d87fffd0
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-22 21:15:22 +0800

    更新 qqreaderAd.js

---
+++ Script/UnlockVip /qqreaderAd.js
@@ -0,0 +1,29 @@
+/*
+General℡
+⚠️脚本仅作为学习，请勿拿去牟利⚠️
+^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3 url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+
+
+hostname = commontgw.reader.qq.com
+
+*/
+
+
+//获取相应数据
+let obj = JSON.parse($response body) ;
+// 获取请求地址
+let requestUrl = $request.url;
+// 判断是否为匹配项
+if (/'^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3/.test(requestUrl)){
+//判断是否存在数据
+if (obj.hasOwnProperty("data"')) {
+
+delete obj.data;//删除指定数据
+
+
+console.log("拦截成功！！！");//打印拦截结果
+}
+
+}
+//重写数据
+$done ({ body: JSON.stringify(obj) });

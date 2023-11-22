48faaa0a46a767029d7af72ca0c8cc7d80b5e00d
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-22 22:19:45 +0800

    更新 qqreaderAd.js

--- Script/UnlockVip /qqreaderAd.js
+++ Script/UnlockVip /qqreaderAd.js
@@ -1,11 +1,15 @@
 /*
 General℡
-QQ阅读去除 我的-活动页面
+QQ阅读去除广告
 
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
 
-^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3 url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreaderAd.js
+^https?:\/\/commontgw\.reader\.qq\.com\/common\/(adV3|firstOpenApp\/ad?) url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreaderAd.js
+
+
+
+
 
 
 hostname = commontgw.reader.qq.com
@@ -18,7 +22,8 @@ let obj = JSON.parse($response.body) ;
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3/.test(requestUrl)){
+if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/adV3/.test(requestUrl))//去除 我的-活动页面
+{
 //判断是否存在数据
 if (obj.hasOwnProperty("data")) {
 
@@ -30,6 +35,16 @@ console.log("拦截成功！！！");//打印拦截结果
 else{
     console.log("拦截失败！！！");
 }
+if (/^https?:\/\/commontgw\.reader\.qq\.com\/common\/firstOpenApp\/ad?/.test(requestUrl))//
+
+{
+    if (obj.hasOwnProperty("sk")) {
+         delete obj.sk.pool;
+        console.log("拦截成功!!!");
+        }
+        else{
+            console.log("拦截失败!!!");
+            }
 }
 //重写数据
 $done ({ body: JSON.stringify(obj) });

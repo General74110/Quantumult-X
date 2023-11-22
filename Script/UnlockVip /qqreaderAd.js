fe1124ddd026e73cfef5763abb66bc5b9f579953
General℡ <76241047+General74110@users.noreply.github.com>
2023-11-22 23:39:16 +0800

    更新 qqreaderAd.js

--- Script/UnlockVip /qqreaderAd.js
+++ Script/UnlockVip /qqreaderAd.js
@@ -5,11 +5,11 @@ QQ阅读去除广告
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
 
-^https?:\/\/api\.jdapi\.com\/sdk\/app\/decide url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreaderAd.js
+^https?:\/\/us\.l\.qq\.com\/exapp\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreaderAd.js
 
 
 
-hostname = api.jdapi.com
+hostname = us.l.qq.com
 
 */
 
@@ -19,19 +19,19 @@ let obj = JSON.parse($response.body) ;
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^https?:\/\/api\.jdapi\.com\/sdk\/app\/decide/.test(requestUrl))//去除 我的-活动页面
+if (/^https?:\/\/us\.l\.qq\.com\/exapp\?/.test(requestUrl))//去除 我的-活动页面
 
 
 {
 //判断是否存在数据
-if (obj.hasOwnProperty("books")) 
+if (obj.hasOwnProperty("data")) 
 
 {
 
-delete obj.books;//删除指定数据
+delete obj.data;//删除指定数据
 
 
-console.log("页面拦截成功！！！");//打印拦截结果
+console.log("开屏广告拦截成功！！！");//打印拦截结果
 
 
 }

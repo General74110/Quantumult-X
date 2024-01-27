87688e9a7761321f5296a8c0d89fdae0a1e01013
General℡ <76241047+General74110@users.noreply.github.com>
2024-01-27 23:04:25 +0800

    更新 qqreader.js

--- Script/UnlockVip /qqreader.js
+++ Script/UnlockVip /qqreader.js
@@ -1,9 +1,10 @@
 /*
-^https:\/\/commontgw\.reader\.qq\.com\/book\/queryBookInfo\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
+^https:\/\/(commontgw|iostgw)\.reader\.qq\.com\/(book\/queryBookInfo|v7_6_6\/paraComment\/myBookParaComment|v7_6_6\/balance)\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/UnlockVip%20/qqreader.js
 
 
 
-hostname=commontgw.reader.qq.com, 
+
+hostname=commontgw.reader.qq.com, iostgw.reader.qq.com
 
 */
 
@@ -14,7 +15,8 @@ var obj = JSON.parse(body);
 
 
 const BookInfo = 'book/queryBookInfo';
-
+const BookPara = 'paraComment/myBookParaComment'
+const Balance ='v7_6_6\/balance'
 
 
 if (url.indexOf(BookInfo) != -1) {
@@ -36,11 +38,35 @@ if (url.indexOf(BookInfo) != -1) {
 	obj ["vipEndTime"] = 4073207338000;
 	obj ["needOpenVip"] = "true";
 	obj ["bsex"] = 1;
-	'
-	
-	
-
 
 body = JSON.stringify(obj);
 
    }
+
+
+
+
+if (url.indexOf(BookPara) != -1) {
+	obj ["refinedWhole"] = 0;
+	obj ["vipEndTime"] = 4073207338000;
+	
+body = JSON.stringify(obj);
+}
+
+
+
+
+if (url.indexOf(Balance) != -1) {
+	obj ["vipLevel"] = 5;
+	obj ["vipEndTime"] = "2099-02-11";
+	obj ["vipType"] = 2;
+	obj ["monthUser"]["vipComment"] = "2099-02-11到期";
+	obj ["monthUser"]["gfrom"] = 31;
+	obj ["monthUser"]["endtime"] = 4074508799;
+	obj ["monthUser"]["vipStatus"] = 1;
+	obj ["monthUser"]["vipButton"] = 1;
+	obj ["coupon"] = 1;
+	obj ["isMVip"] = 1;
+	
+body = JSON.stringify(obj);
+}

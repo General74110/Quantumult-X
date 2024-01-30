d67bb6e983c9eb9aa5664306237a0244211ff15b
General℡ <76241047+General74110@users.noreply.github.com>
2024-01-31 01:44:21 +0800

    更新 Cheji.js

--- Script/UnlockVip /Cheji.js
+++ Script/UnlockVip /Cheji.js
@@ -1,67 +1,25 @@
 /*
 [rewrite_local]
 
-^https:\/\/tsp\.txzing\.com\/api(\/p/operation/common/allowFreeVip|\/p/operation/vipCoupon/popUpCouponInfo|\/p/operation/activity/situation|\/w/wx/module/findCar/findCarVipInfo/isVip|\/w/wx/module/trace/userVip|\/w/wx/module/traceMp/traceUserVipOpenId/vipInfo|\/w\/wx\/module\/smallProgram\/userInfo|\/wx\/module\/smallProgram\/userInfo\/banner) url script-response-body Cheji.js
+^https:\/\/tsp-operation\.txzing\.com\/api\/w\/wx\/module\/traceMp\/traceUserVipOpenId\/vipInfo url script-response-body Cheji.js
 
 
 MITM 
 
-hostname = tsp.txzing.com
+hostname = *.txzing.com
 */
 
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/common/allowFreeVip';
-const dish = '/vipCoupon/popUpCouponInfo';
-const sb = '/activity/situation';
-const bd = '/findCarVipInfo/isVip';
-const gj = '/trace/userVip';
-const gg = '/traceUserVipOpenId/vipInfo';
-const us = '/smallProgram/userInfo';
-const ban = '/userInfo/banner';
+const VipInfo = '/traceUserVipOpenId/vipInfo';
 
-if (url.indexOf(vip) != -1) {
-   obj["data"] = "true";
-   body = JSON.stringify(obj);
-   }
-   
-if (url.indexOf(dish) != -1) {
-obj["data"]["show_cou_pon"] = "1";
-     body = JSON.stringify(obj);
-     }
-
-if (url.indexOf(sb) != -1) {
-	obj["data"]["is_can_participate"] = "true";
-	body = JSON.stringify(obj);
-    }
-    
-if (url.indexOf(bd) != -1) {
-	obj["data"]["pop_up"] = "1";
-	obj["data"]["vip"] = "true"
-	body = JSON.stringify(obj);
-    }
-    
-if (url.indexOf(gj) != -1) {
-	obj["data"]["is_vip"] = "true"
-	body = JSON.stringify(obj);
-    }
-
-if (url.indexOf(gg) != -1) {
+if (url.indexOf(VipInfo) != -1) {
    obj["data"]["vip"] = "true";
-   obj["data"]["day"] = "738639";
-   obj["data"]["can_use_free_vip"] = "false";
+   obj["data"]["day"] = 99999;
    body = JSON.stringify(obj);
    }
    
-if (url.indexOf(us) != -1) {
-   obj["data"]["online"] = "true";
-   body = JSON.stringify(obj);
-   }
-   
-if (url.indexOf(ban) != -1) {
-   obj ['data'][0].id = 4;
-   body = JSON.stringify(obj);
-   }
+
 $done({body});

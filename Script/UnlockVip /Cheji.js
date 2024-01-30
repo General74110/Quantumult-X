d46136a1d5f59c0d19c87459519df3c5f5751813
General℡ <76241047+General74110@users.noreply.github.com>
2024-01-31 02:10:23 +0800

    更新 Cheji.js

--- Script/UnlockVip /Cheji.js
+++ Script/UnlockVip /Cheji.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 
-^https:\/\/tsp-operation\.txzing\.com\/api\/w\/wx\/module\/traceMp\/traceUserVipOpenId\/vipInfo url script-response-body Cheji.js
+^https:\/\/tsp-operation\.txzing\.com\/api\/w\/wx^https:\/\/tsp-operation\.txzing\.com\/api\/w\/wx\/module(\/traceMp\/traceUserVipOpenId\/vipInfo|\/findCar\/findCarVipInfo\/isVip) url script-response-body Cheji.js
 
 
 MITM 
@@ -13,13 +13,18 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const VipInfo = '/traceUserVipOpenId/vipInfo';
+const VipInfo = '/traceUserVipOpenId/vipInfo';//行驶轨迹
+const IsVip = '/findCarVipInfo/isVip';//查找车辆
 
 if (url.indexOf(VipInfo) != -1) {
    obj["data"]["vip"] = "true";
    obj["data"]["day"] = 99999;
    body = JSON.stringify(obj);
    }
-   
+  
+if (url.indexOf(IsVip) != -1) {
+   obj["data"]["vip"] = "true";
+   body = JSON.stringify(obj);
+   }
 
 $done({body});

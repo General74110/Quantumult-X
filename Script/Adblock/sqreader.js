df76418f66b07de7f5619b5b7543929d76041214
General℡ <76241047+General74110@users.noreply.github.com>
2024-02-18 02:06:47 +0800

    更新 sqreader.js

--- Script/Adblock/sqreader.js
+++ Script/Adblock/sqreader.js
@@ -30,15 +30,20 @@ if (/^https:\/\/(ocean\.shuqireader\.com\/api|huichuan\.sm\.cn)(\/ad\/adserver\/
 {
 //判断是否存在数据
 if (obj.hasOwnProperty("data")) 
- eles if (obj.hasOwnProperty("slot_ad"))
+ 
 {
 
 delete obj.data;//删除指定数据
-delete obj.slot_ad;
+
 delete obj.last_ads;
 
 }
+eles if (obj.hasOwnProperty("slot_ad"))
 
+{
+delete obj.slot_ad;
+delete obj.last_ads;
+    }
 
 }
 

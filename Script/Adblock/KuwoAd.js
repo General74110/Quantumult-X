c7237ff43cd54bbb988550a52395357f55e9c058
General℡ <76241047+General74110@users.noreply.github.com>
2024-04-29 22:28:36 +0800

    更新 KuwoAd.js

--- Script/Adblock/KuwoAd.js
+++ Script/Adblock/KuwoAd.js
@@ -29,7 +29,7 @@ let obj = JSON.parse($response.body);
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^http://rich.kuwo.cn/AdService/kaiping/getcache?/.test(requestUrl)) {
+if (/^http:\/\/rich\.kuwo\.cn\/AdService\/kaiping\/getcache\?/.test(requestUrl)) {
     //判断是否存在数据
     if (obj.hasOwnProperty("list")) {
         delete obj.list;//删掉list

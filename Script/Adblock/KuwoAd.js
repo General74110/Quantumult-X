7c2221dd4e8e92091ffe041a7aaf85a50e112101
General℡ <76241047+General74110@users.noreply.github.com>
2024-04-29 21:54:19 +0800

    更新 KuwoAd.js

--- Script/Adblock/KuwoAd.js
+++ Script/Adblock/KuwoAd.js
@@ -3,18 +3,21 @@
 APP: 酷我音乐
 应用: 
 1️⃣去除开屏广告
+2⃣️去除1元开通页面
 
 
-更新时间: 2024.04.26
+更新时间: 2024.04.29
 
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
 
-^http:\/\/rich\.kuwo\.cn\/AdService\/kaiping\/getcache\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/Adblock/KuwoAd.js
+^http:\/\/(rich|wapi)\.kuwo\.cn\/(AdService\/kaiping\/getcache|openapi\/v1\/album\/adBar\/getTsAdConfig)\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/Adblock/KuwoAd.js
 
 
 
-hostname = rich.kuwo.cn
+
+
+hostname = rich.kuwo.cn, wapi.kuwo.cn
 
 */
 
@@ -26,14 +29,18 @@ let requestUrl = $request.url;
 // 判断是否为匹配项
 if (/^http:\/\/rich\.kuwo\.cn\/AdService\/kaiping\/getcache\?/.test(requestUrl))//去除 开屏广告
 
+if (/^http:\/\/wapi\.kuwo\.cn\/openapi\/v1\/album\/adBar\/getTsAdConfig\?/.test(requestUrl))//去除1元开通会员
+
 
 {
 //判断是否存在数据
 if (obj.hasOwnProperty("list")) 
+if (obj.hasOwnProperty("data.tmpPlayList"))
 
 {
 
 delete obj.list;//删除指定数据
+delete obj.data.tmpPlayList;
 delete obj.last_ads;
 
 

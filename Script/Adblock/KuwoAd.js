a0c114963f200116846737e385f5a25f48dff026
General℡ <76241047+General74110@users.noreply.github.com>
2024-04-29 22:22:33 +0800

    更新 KuwoAd.js

--- Script/Adblock/KuwoAd.js
+++ Script/Adblock/KuwoAd.js
@@ -23,33 +23,24 @@ hostname = rich.kuwo.cn, wapi.kuwo.cn
 
 
 //获取相应数据
-let obj = JSON.parse($response.body) ;
+let//获取相应数据
+let obj = JSON.parse($response.body);
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^http:\/\/(rich|wapi)\.kuwo\.cn\/(AdService\/kaiping\/getcache|openapi\/v1\/album\/adBar\/getTsAdConfig)\?/.test(requestUrl))//去除 开屏广告
-//去除1元开通会员
+if (/^http://rich.kuwo.cn/AdService/kaiping/getcache?/.test(requestUrl)) {
+    //判断是否存在数据
+    if (obj.hasOwnProperty("list")) {
+        delete obj.list;//删掉list
+delete obj.last_ads;//删掉last_ads
 
-
-{
-//判断是否存在数据
-if (obj.hasOwnProperty("list")) 
-
-if (obj.hasOwnProperty("data"))
-{
-
-
-
-delete obj.list;//删除指定数据
-delete obj.data;
-delete obj.last_ads;
-
-
-
-}
-
-
- 
+    }
+    
+} else if (/:\/\/wapi\.kuwo\.cn\/openapi\/v1\/album\/adBar\/getTsAdConfig\?/.test(requestUrl)) {
+    if (obj.hasOwnProperty("data")) {
+        delete obj.data;//删掉data
+delete obj.last_ads;//删掉last_ads
+    }
 }
 //重写数据
-$done ({ body: JSON.stringify(obj) });
+$done ({body: JSON.stringify(obj)});

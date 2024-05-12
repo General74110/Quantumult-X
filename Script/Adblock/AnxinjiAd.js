2ff315308a6efb063449f7445bf0d0f82081dec4
General℡ <76241047+General74110@users.noreply.github.com>
2024-05-12 15:10:02 +0800

    更新 AnxinjiAd.js

--- Script/Adblock/AnxinjiAd.js
+++ Script/Adblock/AnxinjiAd.js
@@ -35,13 +35,13 @@ if (obj.hasOwnProperty("results"))
 
 delete obj.results;//删除指定数据
 
-console.log("安心记加班:", 开屏广告删除成功);//输出日志
+console.log("安心记加班:", "","开屏广告删除成功);//输出日志"
 
 
 
 
-} else{
-    console.log("安心记加班:","开屏广告删除失败",Error occurred: ${error}
+} else {
+    console.log("安心记加班:","开屏广告删除失败","Error occurred: ${error}"
 }
  
  

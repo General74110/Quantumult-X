ccf9e966c3c07c03b3a4313c4e0165486bfb6ef9
General℡ <76241047+General74110@users.noreply.github.com>
2024-05-12 22:54:03 +0800

    更新 AnxinjiAd.js

--- Script/Adblock/AnxinjiAd.js
+++ Script/Adblock/AnxinjiAd.js
@@ -10,22 +10,20 @@ APP: 安心记加班
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
 
-^https?:\/\/(jjbapi\.julanling\.com\/advertConfig\/queryCutOverScreenConfig|api-access\.pangolin-sdk-toutiao1\.com\/api\/ad\/union\/sdk\/stats\/batch)\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/Adblock/AnxinjiAd.js
+^https?:\/\/jjbapi\.julanling\.com(\/advertConfig\/queryCutOverScreenConfig|splash_screen\/jjb_splash_screen_v3)\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/Adblock/AnxinjiAd.js
 
 
 
-hostname = jjbapi.julanling.com, api-access.pangolin-sdk-toutiao1.com
+hostname = jjbapi.julanling.com
 
 */
 
-
 //获取相应数据
-let obj = JSON.parse($response.body);
-
+let obj = JSON.parse($response.body) ;
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^https?:\/\/jjbapi\.julanling\.com\/advertConfig\/queryCutOverScreenConfig\?/.test(requestUrl))//去除 开屏广告
+if (/^https?:\/\/jjbapi\.julanling\.com(\/advertConfig\/queryCutOverScreenConfig|splash_screen\/jjb_splash_screen_v3)\?/.test(requestUrl))//去除 开屏广告/我的-全部活动页面入口
 
 
 {
@@ -36,11 +34,10 @@ if (obj.hasOwnProperty("results"))
 
 delete obj.results;//删除指定数据
 
-consoles.log("安心记加班:", "开屏广告删除成功","");//输出日志
+
 
 }
-
  
 }
 //重写数据
-$done ({ body: JSON.stringify(obj)});
+$done ({ body: JSON.stringify(obj) });

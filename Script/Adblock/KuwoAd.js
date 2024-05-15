5b7469f90e609670debc378d2d434fe255f82fe2
General℡ <76241047+General74110@users.noreply.github.com>
2024-05-15 23:00:46 +0800

    更新 KuwoAd.js

--- Script/Adblock/KuwoAd.js
+++ Script/Adblock/KuwoAd.js
@@ -6,12 +6,12 @@ APP: 酷我音乐
 2⃣️去除1元开通页面
 
 
-更新时间: 2024.04.29
+更新时间: 2024.05.15
 
 ⚠️脚本仅作为学习，请勿拿去牟利⚠️
 
 
-^http:\/\/(rich|wapi)\.kuwo\.cn\/(AdService\/kaiping\/getcache|openapi\/v1\/album\/adBar\/getTsAdConfig)\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/Adblock/KuwoAd.js
+^http:\/\/(rich|wapi)\.kuwo\.cn\/(AdService\/kaiping\/(getcache|adinfo)|openapi\/v1\/album\/adBar\/getTsAdConfig)\? url script-response-body https://raw.githubusercontent.com/General74110/Quantumult-X/master/Script/Adblock/KuwoAd.js
 
 
 
@@ -20,8 +20,6 @@ APP: 酷我音乐
 hostname = rich.kuwo.cn, wapi.kuwo.cn
 
 */
-
-
 
 
 //获取相应数据
@@ -33,15 +31,26 @@ if (/^http:\/\/rich\.kuwo\.cn\/AdService\/kaiping\/getcache\?/.test(requestUrl))
     //判断是否存在数据
     if (obj.hasOwnProperty("list")) {
         delete obj.list;//删掉list
-delete obj.last_ads;//删掉last_ads
+
 
     }
     
 } else if (/^http:\/\/wapi\.kuwo\.cn\/openapi\/v1\/album\/adBar\/getTsAdConfig\?/.test(requestUrl)) {
     if (obj.hasOwnProperty("data")) {
         delete obj.data;//删掉data
-delete obj.last_ads;//删掉last_ads
-    }
+
+    } 
+    }else if (/^http:\/\/rich\.kuwo\.cn\/AdService\/kaiping\/adinfo\?/.test(requestUrl)) {
+    //判断是否存在数据
+    if (obj.hasOwnProperty("adType")) {
+        delete obj.screenClick;//删掉screenClick
+        delete obj.click_conf;//删掉click_conf
+        delete obj.url;//删掉url
+        
+        delete obj.count_id;//删掉count_id
+        
+}
+
 }
 
 
